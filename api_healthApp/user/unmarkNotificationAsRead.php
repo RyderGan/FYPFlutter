341b2865ec2b85d5bbff5366a52f0f8fb8302097
Gan Jone Wan <ganwan2@gmail.com>
2024-01-13 11:49:46 +0800

    Added more backend php and updated sql file

---
+++ api_healthApp/user/unmarkNotificationAsRead.php
@@ -0,0 +1,18 @@
+<?php
+include '../connection.php';
+
+//POST = send/save data
+//GET retrieve/read data
+
+$notifyID = $_POST['notifyID'];
+$userID = $_POST['userID'];
+$hasRead = 0;
+
+$sqlQuery = "UPDATE notifications SET hasRead = '$hasRead' WHERE notify_id = '$notifyID' AND user_id = '$userID'";
+$result = $connectNow->query($sqlQuery);
+
+if($result){
+    echo json_encode(array("success"=>true));
+}else{
+    echo json_encode(array("success"=>false));
+}
